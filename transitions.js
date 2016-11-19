72d151f00a0a466bf2106074383fa934a2f91b19
Wing Hung Leung <hudsonleung95@gmail.com>
2016-11-19 05:28:09 -0800

    Added transition for project pic; fixed cover string; modified intro

--- transitions.js
+++ transitions.js
@@ -1,6 +1,9 @@
 var $animation_ele = $('.animat');
+var $icon = $('#projs .icon');
+var $projpic = $('#projs .pic');
 var $window = $(window);
 
+//nav bar, scroll effect
 jQuery(document).ready(function ($) {
 
     $("nav a").click(function (event) {
@@ -9,6 +12,7 @@ jQuery(document).ready(function ($) {
     });
 });
 
+//track in view obj
 function check_in_view() {
     var window_height = $window.height();
     var window_top_position = $window.scrollTop();
@@ -28,6 +32,55 @@ function check_in_view() {
             $element.removeClass('in-view');
         }
     });
+}
+
+function clearClass() {
+    var pic = document.getElementById('projpic');
+
+    if (pic.classList.contains('icon1'))
+        $projpic.removeClass('icon1');
+
+    if (pic.classList.contains('icon2'))
+        $projpic.removeClass('icon2');
+
+    if (pic.classList.contains('icon3'))
+        $projpic.removeClass('icon3');
+
+    if (pic.classList.contains('icon4'))
+        $projpic.removeClass('icon4');
+}
+
+function showPic(id) {
+    clearClass();
+
+    switch (id) {
+        case 1:
+            $projpic.addClass('icon1');
+            break;
+        case 2:
+            $projpic.addClass('icon2');
+            break;
+        case 3:
+            $projpic.addClass('icon3');
+            break;
+        case 4:
+            $projpic.addClass('icon4');
+            break;
+    }
+
+    
+
+    
+}
+
+function moveIcon(id) {
+
+    $projpic.fadeOut(1, showPic(id));
+
+    $projpic.fadeIn();
+
+    $projpic.addClass('selected');
+
 }
 
 $window.trigger('scroll');
